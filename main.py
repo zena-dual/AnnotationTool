1a1da030657c6a5c8d8a2799a698203707c28229
zena-dual <daikith14@gmail.com>
2016-02-03 00:35:33 +0900

    コードを整理

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import Tkinter
 import tkFont
 from ScrolledText import *
 
-index = ""
 
+# タグと属性の組み合わせをresource/tag_attributeから読み込む関数
+# tag_list: タグ名のリスト
+# attribute_dict: キーとしてタグ名、値として属性を持つ辞書
+tag_list = []
+attribute_dict = {}
 def read_tag_and_attribute():
     tag_attribute = open("resource/tag_attribute", "r")
-    tag_list = []
-    attribute_dict = {}
     
     for line in tag_attribute:
         string = line.replace("\n", "").split(",")
-        print string
         tag_list.append(string[0])
         if len(line) > 1:
             attribute_dict[string[0]] = []
@@ -24,6 +25,15 @@ def read_tag_and_attribute():
                 i += 1
     
     return tag_list, attribute_dict
+
+
+# 左側のテキストをドラッグで選択し、選択したテキストを右側のテキストに送る関数
+def index_capture(hoge):
+    t1.insert(Tkinter.END,t0.get(t0.index(Tkinter.SEL_FIRST),\
+                                 t0.index(Tkinter.SEL_LAST)))
+    t1.insert(Tkinter.END, "\t")
+    t1.insert(Tkinter.END, "hoge")
+    t1.insert(Tkinter.END, "\n")
     
 
 if __name__ == "__main__":
@@ -44,23 +54,11 @@ if __name__ == "__main__":
     t0.place(x=10,y=10)
     t1.place(x=600,y=10)
 
-    # 左側のテキストにファイルの出力を反映
+    # 左側のテキストにファイルの内容を反映
     for element in text:
         t0.insert(Tkinter.END, "　" + element)
 
-    # 左側のテキストをドラッグで選択し、選択したテキストを右側のテキストに送る関数
-    def index_capture():
-        global index
-        if index == "":
-            index = t0.index(Tkinter.CURRENT)
-        else:
-            t1.insert(Tkinter.END, t0.get(index, t0.index(Tkinter.CURRENT)))
-            t1.insert(Tkinter.END, "\t")
-            t1.insert(Tkinter.END, "hoge")
-            t1.insert(Tkinter.END, "\n")
-            index = ""
-
-    t0.bind("<Button-1>", index_capture)
+    # ドラッグ時の挙動をbind
     t0.bind("<ButtonRelease>", index_capture)
 
     # "Choose tag to annotate"ラベルを表示
@@ -70,30 +68,24 @@ if __name__ == "__main__":
                                    background="#b0c4de")
     TagChoiceLabel.place(x=70,y=430)
 
-    # タグと属性の組み合わせをresource/tag_attributeから読み込み
-    # tag_list: タグ名のリスト
-    # attribute_dict: キーとしてタグ名、値として属性を持つ辞書
-    tag_list = []
-    attribute_dict = {}
-    tag_list, attribute_dict = read_tag_and_attribute()
-
     # アノテーションするタグの選択
+    read_tag_and_attribute()
     tag_rb_list = []
     i = 0
-    X = 10
-    Y = 455
+    X_rb = -180
+    Y_rb = 455
     while i < len(tag_list):
-        tag_rb_list.append(Tkinter.Radiobutton(text=tag_list[i],\
-                                               variable=tagID,\
-                                               value=i))
         # ラジオボタンの位置調整
-        if i == 5:
-            X += 190
-            Y = 455
-            
-        tag_rb_list[i].place(x=X,y=Y)
-        Y += 20
+        if i % 5 == 0:
+            X_rb += 190
+            Y_rb = 455
+        
+        tag_rb_list.append(Tkinter.Radiobutton(text = tag_list[i],\
+                                               variable = tagID,\
+                                               value = i))
+        tag_rb_list[i].place(x=X_rb,y=Y_rb)
+        Y_rb += 20
         i += 1
-    
+
     # 実行処理はここまで
     root.mainloop()
